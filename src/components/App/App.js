028d87f24754fb7cfdb193baf56a950ad3055385
rajesh2724 <rajeshsri1436@gmail.com>
2024-06-26 19:32:43 +0530

    day 4 modified

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from '../Nav/NavBar';
 import Slider from '../Sliders/Sliders';
+import Signup from '../Signup/Signup';
 
 function App() {
   return (
     <BrowserRouter>
     <NavBar/>
+    <Slider/>
       <Routes>
         <Route path='/' element={<App/>}/>
+        <Route path='logup' element={<Signup/>}/>
       </Routes>
-      <Slider/>
     </BrowserRouter>
   );
 }
