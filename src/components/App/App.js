b4aa27c8036c7a53410bcf28e20ad8ee22f147be
rajesh2724 <rajeshsri1436@gmail.com>
2024-07-01 19:32:07 +0530

    day5 modified

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,16 +2,13 @@ import './App.css';
 
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from '../Nav/NavBar';
-import Slider from '../Sliders/Sliders';
 import Signup from '../Signup/Signup';
 import New from './New';
 function App() { 
-  let  name="rajesh"
   return (
     <div> 
       <Routes> 
-        <Route path='/' element={<New name={name}/>}/>
+        <Route path='/' element={<New/>}/>
         <Route path='/signup' element={<Signup/>}/>
       
       </Routes>
