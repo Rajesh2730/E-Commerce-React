3bf4c166a8ba9430457f8e48afadc9292ba0162f
rajesh2724 <rajeshsri1436@gmail.com>
2024-07-16 21:49:43 +0530

    Updates

--- src/components/Signin/SignIn.js
+++ src/components/Signin/SignIn.js
@@ -3,62 +3,79 @@ import { Form, Button, Row, Col, InputGroup } from 'react-bootstrap';
 import FloatingLabel from 'react-bootstrap-floating-label';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-import './signin.css'
+import axios from 'axios';
+import './signin.css';
 import NavBar from '../Nav/NavBar';
+import { useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
+  const navigate = useNavigate()
   const [showPassword, setShowPassword] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState('');
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Handle form submission here
-    console.log({  username, password });
+    try {
+      const response = await axios.post('http://localhost:7230/login', { username, password });
+      console.log('Sign in successful:', response.data);
+      navigate("/E-Commerce-React")
+      
+    } catch (error) {
+      console.error('Error signing in:', error);
+      setErrors('Invalid username or password');
+    }
   };
 
   return (
     <div className='glass-bg'>
-        <NavBar/>
-    <div className='container'>
-      <Row className="w-100">
-        <Col md={6} lg={4} className="mx-auto">
-          <h3 className="text-center mb-4">Sign In</h3>
-          <Form onSubmit={handleSubmit}>
-            <FloatingLabel controlId="formBasicUsername" label="Username" className="mb-3">
-              <Form.Control
-                type="text"
-                placeholder="Enter username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </FloatingLabel>
-
-            <FloatingLabel controlId="formBasicPassword" label="Password" className="mb-3">
-              <InputGroup>
+      <NavBar/>
+      <div className='container'>
+        <Row className="w-100">
+          <Col md={6} lg={4} className="mx-auto">
+            <h3 className="text-center mb-4">Sign In</h3>
+            <Form onSubmit={handleSubmit}  >
+              <FloatingLabel controlId="formBasicUsername" label="Username" className="mb-3">
                 <Form.Control
-                  type={showPassword ? 'text' : 'password'}
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
+                  type="text"
+                  placeholder="Enter username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  isInvalid={!!errors}
+                  required
                 />
-                <InputGroup.Text onClick={togglePasswordVisibility} className="eye-icon">
-                  <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
-                </InputGroup.Text>
-              </InputGroup>
-            </FloatingLabel>
+                <Form.Control.Feedback type="invalid">{errors}</Form.Control.Feedback>
+              </FloatingLabel>
 
-            <Button variant="primary" type="submit" className="w-100 mt-3">
-              Submit
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </div>
+              <FloatingLabel controlId="formBasicPassword" label="Password" className="mb-3">
+                <InputGroup>
+                  <Form.Control
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    isInvalid={!!errors}
+                    required
+                  />
+                  <InputGroup.Text onClick={togglePasswordVisibility} className="eye-ico">
+                    <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+                  </InputGroup.Text>
+                  <Form.Control.Feedback type="invalid">{errors}</Form.Control.Feedback>
+                </InputGroup>
+              </FloatingLabel>
+
+              <Button variant="primary" type="submit" className="w-100 mt-3">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 import NavBar from '../Nav/NavBar';
 import './signup.css';
-import { Form,InputGroup } from 'react-bootstrap';
+import { Form, InputGroup } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+  const navigate = useNavigate()
   const [formData, setFormData] = useState({
     username: '',
     email: '',
@@ -16,6 +18,7 @@ const Signup = () => {
   });
 
   const [errors, setErrors] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -41,104 +44,112 @@ const Signup = () => {
     return newErrors;
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const validationErrors = validateForm();
     if (Object.keys(validationErrors).length === 0) {
-      console.log('Form data submitted:', formData);
+      try {
+        const response = await axios.post('http://localhost:7230/signup', formData);
+        console.log('Form data submitted:', response.data);
+        navigate('/login')
+        alert("You Are Important To Us.Please LogIn To Explore Our Platform ;)")
+      } catch (error) {
+        console.error('Error submitting form:', error);
+      }
     } else {
       setErrors(validationErrors);
     }
   };
 
-  const [showPassword, setShowPassword] = useState(false);
-
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <div className='glass-bg'>
-    <div className="container">
-      <h2 className="my-4 row justify-content-center">Sign Up</h2>
-      <form onSubmit={handleSubmit} noValidate>
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-lg-4">
-            <label htmlFor="username" className="form-label">Username</label>
-            <input
-              type="text"
-              className={`form-control ${errors.username ? 'is-invalid' : ''}`}
-              id="username"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              autoComplete='off'
-              required
-            />
-          </div>
-        </div>
-        <div className="mb-3 row justify-content-center">
-          <div className="col-md-6 col-lg-4">
-            <label htmlFor="email" className="form-label">Email</label>
-            <input
-              type="email"
-              className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              autoComplete='off'
-              required
-            />
-          </div>
-        </div>
-        <div className="mb-3 row justify-content-center">
-          <div className="col-md-6 col-lg-4">
-            <label htmlFor="password" className="form-label">Password</label>
-            <InputGroup>
-                <Form.Control
-                  type={showPassword ? 'text' : 'password'}
-                  value={formData.password}
+        <div className="container">
+          <h2 className="my-4 row justify-content-center">Sign Up</h2>
+          <form onSubmit={handleSubmit} noValidate>
+            <div className="row justify-content-center">
+              <div className="col-md-6 col-lg-4">
+                <label htmlFor="username" className="form-label">Username</label>
+                <input
+                  type="text"
+                  className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+                  id="username"
+                  name="username"
+                  value={formData.username}
                   onChange={handleChange}
-                  id='password'
-                  name='password'
+                  autoComplete='off'
                   required
                 />
-            <InputGroup.Text onClick={togglePasswordVisibility} className="eye-icon">
-                  <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
-                </InputGroup.Text>
-                </InputGroup>
-          </div>
-        </div>
-        <div className="mb-5 row justify-content-center">
-          <div className="col-md-6 col-lg-4">
-            <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
-            <InputGroup>
-                <Form.Control
-                  type={showPassword ? 'text' : 'password'}
-                  value={formData.confirmPassword}
+                {errors.username && <div className="invalid-feedback">{errors.username}</div>}
+              </div>
+            </div>
+            <div className="mb-3 row justify-content-center">
+              <div className="col-md-6 col-lg-4">
+                <label htmlFor="email" className="form-label">Email</label>
+                <input
+                  type="email"
+                  className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                  id="email"
+                  name="email"
+                  value={formData.email}
                   onChange={handleChange}
-                  id='confirmPassword'
-                  name='confirmPassword'
+                  autoComplete='off'
                   required
                 />
-            <InputGroup.Text onClick={togglePasswordVisibility} className="eye-icon">
-                  <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
-                </InputGroup.Text>
+                {errors.email && <div className="invalid-feedback">{errors.email}</div>}
+              </div>
+            </div>
+            <div className="mb-3 row justify-content-center">
+              <div className="col-md-6 col-lg-4">
+                <label htmlFor="password" className="form-label">Password</label>
+                <InputGroup>
+                  <Form.Control
+                    type={showPassword ? 'text' : 'password'}
+                    value={formData.password}
+                    onChange={handleChange}
+                    id='password'
+                    name='password'
+                    required
+                  />
+                  <InputGroup.Text onClick={togglePasswordVisibility} className="eye-icon">
+                    <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+                  </InputGroup.Text>
                 </InputGroup>
-          </div>
+                {errors.password && <div className="invalid-feedback">{errors.password}</div>}
+              </div>
+            </div>
+            <div className="mb-5 row justify-content-center">
+              <div className="col-md-6 col-lg-4">
+                <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
+                <InputGroup>
+                  <Form.Control
+                    type={showPassword ? 'text' : 'password'}
+                    value={formData.confirmPassword}
+                    onChange={handleChange}
+                    id='confirmPassword'
+                    name='confirmPassword'
+                    required
+                  />
+                  <InputGroup.Text onClick={togglePasswordVisibility} className="eye-icon">
+                    <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+                  </InputGroup.Text>
+                </InputGroup>
+                {errors.confirmPassword && <div className="invalid-feedback">{errors.confirmPassword}</div>}
+              </div>
+            </div>
+            <div className="mb-3 row justify-content-center">
+              <div className="col-md-6 col-lg-4">
+                <button type="submit" className="btn btn-primary w-100">Sign Up</button>
+              </div>
+            </div>
+          </form>
         </div>
-        <div className="mb-3 row justify-content-center">
-          <div className="col-md-6 col-lg-4">
-            <button type="submit" className="btn btn-primary w-100">Sign Up</button>
-          </div>
-        </div>
-      </form>
-    </div>
-    </div>
+      </div>
     </div>
   );
 };
